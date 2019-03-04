ed8f32ad711c19c254dc93c97d89d19f7a4819c6
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-04 08:44:33 -0500

    install waitress for serving

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,15 +35,19 @@ def runner(app):
 	return app.test_cli_runner()
 
 class AuthActions(object):
-	def __init__(self, client):
-		self._client = client
+  def __init__(self, client):
+      self._client = client
 
-	def login(self, username='test', password='test'):
-		return self._client.post('/auth/login', data={'username': username, 'password': password})
+  def login(self, username='test', password='test'):
+      return self._client.post(
+          '/auth/login',
+          data={'username': username, 'password': password}
+      )
 
-	def logout(self):
-		return self._client.get('/auth/logout')
+  def logout(self):
+      return self._client.get('/auth/logout')
+
 
 @pytest.fixture
 def auth(client):
-	return AuthActions(client)
+  return AuthActions(client)
